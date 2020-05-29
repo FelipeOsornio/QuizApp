0b0a30eb2dd012d52a20dd9370da06e497a93dbe
SwimmerRafa <rafaelmorenoc97@gmail.com>
2020-05-29 18:41:20 +0000

    Player, User Classes

--- models/player.rb
+++ models/player.rb
@@ -2,13 +2,17 @@ require 'json'
 require_relative './print'
 
 class Player < Print
-  attr_accessor
+  attr_accessor :user, :score
 
   def initialize(json)
-
+    data_hash = JSON.parse(json)
+    @user = data_hash["username"]
+    @score = data_hash["score"]
   end
-
+  
+  
   def print
-    ""
+    "Player { username => #{@user}, score => #{@score} }" 
   end
+
 end
--- models/user.rb
+++ models/user.rb
@@ -2,14 +2,23 @@ require 'json'
 require_relative './print'
 
 class User < Print
-  attr_accessor
+  attr_accessor :user, :pass
 
   def initialize(json)
-
+    data_hash = JSON.parse(json)
+    @user = data_hash["user"]
+    @pass = data_hash["pass"]
   end
-
+  
+  def hide
+    encrypted = ""
+    @pass.each_char {|i| encrypted += "*"}
+    encrypted
+  end
+  
   def print
-    ""
+    "Question { user => #{@user}, pass => #{hide} }" 
   end
+  
 end
 
